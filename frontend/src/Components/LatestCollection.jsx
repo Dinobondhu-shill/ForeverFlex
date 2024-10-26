5e403a70aad8d6da3347c5371eb9eb3cfa50c132
Dinobondhu-shill <dinu.webdev@gmail.com>
2024-10-26 13:04:08 +0600

    latest collections added

--- frontend/src/Components/LatestCollection.jsx
+++ frontend/src/Components/LatestCollection.jsx
@@ -1,17 +1,28 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Title from './Title';
 import { ProductContext } from '../Context/ProductContext';
 import ProductItem from './ProductItem';
 
 const LatestCollection = () => {
   const {products} = useContext(ProductContext);
-  console.log(products)
+  const [LatestCollection, setLatestCollection]= useState([])
+
+useEffect(()=>{
+  setLatestCollection(products.slice(0,10))
+},[products])
+
   return (
     <div className='py-4 md:py-6'>
      <Title text1={'LATEST'} text2={'COLLECTIONS'} />
       <p className='text-center  text-[16px] py-3'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi cum, molestias sit minima ab sed.</p>
-      <div>
-        <ProductItem />
+
+      {/* Rendering Latest collections */}
+      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6 mt-8'>
+    {
+      LatestCollection.map((item, index) => 
+      <ProductItem key={index} item={item}/>
+      )
+    }        
       </div>
     </div>
   );
--- frontend/src/Components/ProductItem.jsx
+++ frontend/src/Components/ProductItem.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import  { useContext } from 'react';
+import { ProductContext } from '../Context/ProductContext';
+import { Link } from 'react-router-dom';
+import { assets } from '../../public/assets/frontend_assets/assets';
 
-const ProductItem = () => {
+
+const ProductItem = ({item}) => {
+  const {currency,} = useContext(ProductContext)
+  const {name, price, _id, image, description} = item
+  
   return (
-    <div>
-     
-    </div>
+    <Link to={`/product/${_id}`} className='text-gray-700 cursor-pointer'>
+     <div className='overflow-hidden'>
+    <img className='hover:scale-110 transition ease-in-out ' src={image[0]} alt="" />
+     </div>
+     <div className='spacey-1'>
+      <h2 className='text-[18px] text-black pt-3'>{name}</h2>
+      <p>Price: {currency}{price}</p>
+     </div>
+    </Link>
   );
 };
 
