35503fdb43d58bb8a7d87caa170ea2a65c7570e0
Dinobondhu-shill <dinu.webdev@gmail.com>
2024-10-26 13:22:53 +0600

    bestseller product added

--- frontend/src/Components/BestSeller.jsx
+++ frontend/src/Components/BestSeller.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Title from './Title';
+import { ProductContext } from '../Context/ProductContext';
+import ProductItem from './ProductItem';
 
 const BestSeller = () => {
+  const {products} = useContext(ProductContext)
+  const [bestSeller, setBestSeller] = useState([]);
+  
+  useEffect(()=>{
+const bestproduct = products.filter((item)=>item.bestseller)
+setBestSeller(bestproduct.slice(0,5))
+  },[products]);
   return (
-    <div className='py-4 md:py-6'>
+    <div className='py-4 md:py-8'>
      <Title text1={'BEST'} text2={'SELLER'} />
         <p className='text-center font-normal text-[16px] py-3'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi cum, molestias sit minima ab sed.</p>
+
+        {/* render bestseller collection */}
+        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 mt-8'>
+        {
+          bestSeller.map((item, index)=>(
+            <ProductItem key={index} item={item} />
+          ))        }
+        </div>
+        
     </div>
   );
 };
