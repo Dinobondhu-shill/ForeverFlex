b7eacb77bc902bea6f9fc4994d77c7e72f6f23d1
Dinobondhu-shill <dinu.webdev@gmail.com>
2024-10-26 15:38:53 +0600

    product collections pages product added

--- frontend/src/Components/Title.jsx
+++ frontend/src/Components/Title.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Title = ({text1, text2}) => {
   return (
     <div className='flex items-center gap-2 justify-center '>
-        <p className='font-medium text-3xl text-gray-700 '><span className='text-gray-500'>{text1}</span> {text2}</p>
+        <p className='font-medium text-3xl text-gray-600 '><span className='text-gray-500'>{text1}</span> {text2}</p>
         <hr className='w-14 h-[2.5px] bg-black '/>
         </div>
   );
--- frontend/src/Pages/Collection/Collection.jsx
+++ frontend/src/Pages/Collection/Collection.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import Title from '../../Components/Title';
+import { ProductContext } from '../../Context/ProductContext';
+import ProductItem from '../../Components/ProductItem';
 
 const Collection = () => {
+  const {products} = useContext(ProductContext)
   return (
-    <div className='my-5 md:my-10 flex '>
+    <div className='my-5 md:my-10 flex gap-10'>
      {/* Filter section */}
      <div className='max-w-56 flex flex-col gap-5'>
       <h3 className='text-2xl'>FILTERS</h3>
@@ -42,7 +46,27 @@ const Collection = () => {
       </div>
       </div>
      </div>
-     {/*  */}
+     {/* products sections */}
+     <div  className='flex-1'>
+      <div className='flex justify-between items-center'>
+        <Title text1={"ALL"} text2={"COLLECTIONS"} />
+        <div>
+          <select className='p-4 border text-gray-700 outline-none text-[15px]'>
+            <option value="Relevent">Sort By: Relevent</option>
+            <option value="Low to Higt">Sort By: Low to High</option>
+            <option value="High to Low">Sort By: High to Low</option>
+          </select>
+        </div>
+      </div>
+      {/* Render all the product here */}
+      <div className='my-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+    {
+      products.map((item, index)=>(
+        <ProductItem key={index} item={item}/>
+      ))
+    }
+      </div>
+     </div>
     </div>
   );
 };
