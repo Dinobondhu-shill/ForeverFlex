53190714d7524c9fcfea561e15a3d36bd1e4efdb
Dinobondhu-shill <dinu.webdev@gmail.com>
2024-10-30 01:23:56 +0600

    custom photo gallery added

--- frontend/src/Pages/Collection/ProductDetails.jsx
+++ frontend/src/Pages/Collection/ProductDetails.jsx
@@ -24,23 +24,41 @@ useEffect(()=>{
 },[product])
 
   return (
-    <div className='flex flex-col md:flex-row gap-10 mt-5 md:mt-10 w-full'>
-      <div className='flex flex-col-reverse md:flex-row gap-5 w-full md:w-1/2'>
-        <div className='flex flex-row md:flex-col gap-5 w-1/5'>
+    <div className='flex flex-col md:flex-row gap-10 my-5 md:my-10 w-full'>
+      <div className='flex flex-col-reverse md:flex-row gap-6 w-full md:w-1/2'>
+        <div className='flex flex-row md:flex-col gap-3 w-1/5'>
           {
             product?.image.map((item, index) => <div key={index}>
+              <div onClick={()=> setPhoto(item)}>
               <img src={item} alt="" />
+              </div>
             </div>)
           }
         </div>
-        <div>
-          <img src={photo} alt="" />
+        <div className='h-full'>
+          <img src={photo}  className='object-cover'/>
         </div>
       </div>
       <div className='w-full md:w-1/2'>
           <h2 className='text-2xl font-medium'>{product?.name}</h2>
           <p className='text-2xl font-medium my-8'>{currency}{product?.price}</p>
-          <p>{product?.description}</p>
+          <p className='w-3/4 text-gray-500 mb-10'>{product?.description}</p>
+          <div>
+            <h3 className='text-[18px]'>Select Size</h3>
+          <div className='flex gap-3 my-4'>
+            {
+              product?.sizes?.map((item, index)=> <p key={index} className='px-4 py-2 border-[1px] border-gray-500 bg-gray-200'>{item}</p>)
+            }
+          </div>
+          </div>
+          <button className='bg-black text-white px-8 py-2 mt-3 mb-8'>Add To Cart</button>
+          <hr />
+          <div className='text-[15px] space-y-[2px] mt-2 text-gray-400'>
+            <p>100% Original product.</p>
+            <p>Cash on delivery is available on this product.</p>
+            <p>
+            Easy return and exchange policy within 7 days.</p>
+          </div>
       </div>
     </div>
   );
