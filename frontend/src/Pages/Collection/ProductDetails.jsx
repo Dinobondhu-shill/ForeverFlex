b5ad5f9b0b296f70f5c8e4841e4ff6f92ee0cfe6
Dinobondhu-shill <dinu.webdev@gmail.com>
2024-10-30 01:04:54 +0600

    product details and photo clicking added

--- frontend/src/Pages/Collection/ProductDetails.jsx
+++ frontend/src/Pages/Collection/ProductDetails.jsx
@@ -1,15 +1,47 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { ProductContext } from '../../Context/ProductContext';
 
 const ProductDetails = () => {
 const {productId} = useParams();
-const {products} = useContext(ProductContext);
+const {products, currency} = useContext(ProductContext);
+const [product, setProduct] = useState(null);
+const [photo, setPhoto] = useState(null);
+console.log(product)
 
+const fatchedProductData = async()=>{
+ const selectedProduct = products.find(item => item._id== productId)
+ if(selectedProduct){
+  setProduct(selectedProduct)
+ }
+}
+useEffect(()=>{
+fatchedProductData()
+},[products, productId]);
+
+useEffect(()=>{
+  setPhoto(product?.image[0])
+},[product])
 
   return (
-    <div>
-      
+    <div className='flex flex-col md:flex-row gap-10 mt-5 md:mt-10 w-full'>
+      <div className='flex flex-col-reverse md:flex-row gap-5 w-full md:w-1/2'>
+        <div className='flex flex-row md:flex-col gap-5 w-1/5'>
+          {
+            product?.image.map((item, index) => <div key={index}>
+              <img src={item} alt="" />
+            </div>)
+          }
+        </div>
+        <div>
+          <img src={photo} alt="" />
+        </div>
+      </div>
+      <div className='w-full md:w-1/2'>
+          <h2 className='text-2xl font-medium'>{product?.name}</h2>
+          <p className='text-2xl font-medium my-8'>{currency}{product?.price}</p>
+          <p>{product?.description}</p>
+      </div>
     </div>
   );
 };
